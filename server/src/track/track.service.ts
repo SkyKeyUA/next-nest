85e44b1f9c43aa095c58965cf13d4c11ebabf94b
SkyKeyUA <veremiienkoov96@gmail.com>
2023-07-26 16:09:38 +0300

    basic structure for the track is created

---
+++ server/src/track/track.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class TrackService {
+  async create() {}
+
+  async getAll() {}
+
+  async getOne() {}
+  async delete() {}
+}
