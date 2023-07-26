cb00975ff64256896eaa48a9831fedca6465ad65
SkyKeyUA <veremiienkoov96@gmail.com>
2023-07-26 17:38:54 +0300

    npm i --save @nestjs/config

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TrackModule } from './track/track.module';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TrackModule],
+  imports: [
+    ConfigModule.forRoot({
+      //envFilePath: ['.env.development.local', '.env.development'],
+      //isGlobal: true,
+      //ignoreEnvVars: true,
+    }),
+    MongooseModule.forRoot(process.env.DB_URL),
+    TrackModule,
+  ],
 })
 export class AppModule {}
