684e61077866b9cb9ede763576f5a898d4cf7257
SkyKeyUA <veremiienkoov96@gmail.com>
2023-07-26 19:12:02 +0300

    track(dto and create) was created

--- server/src/track/track.controller.ts
+++ server/src/track/track.controller.ts
@@ -1,8 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { TrackService } from './track.service';
+import { CreateTrackDto } from './dto/create-track.dto';
 
 @Controller('/tracks')
 export class TrackController {
-  create() {}
+  constructor(private trackService: TrackService) {}
+  @Post()
+  create(@Body() dto: CreateTrackDto) {
+    return this.trackService.create(dto);
+  }
 
   @Get()
   getAll() {
