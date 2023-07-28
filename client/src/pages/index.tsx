9135c192ee1bf1ff3028af1e31221fb953a06db8
SkyKeyUA <veremiienkoov96@gmail.com>
2023-07-28 15:23:51 +0300

    next start

---
+++ client/src/pages/index.tsx
@@ -0,0 +1,16 @@
+/** @format */
+
+import React from 'react';
+
+const index = () => {
+  return (
+    <>
+      <div className="center">
+        <h1>Home Page</h1>
+        <h3>The best track in the world!</h3>
+      </div>
+    </>
+  );
+};
+
+export default index;
